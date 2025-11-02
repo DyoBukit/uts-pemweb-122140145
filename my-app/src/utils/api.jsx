a428fd827d49ef665e7a6b8e759b25781f9ee5d8
DyoBukit <diocarol123@gmail.com>
2025-11-02 13:51:18 +0700

    Pembuatan folders home dan api serta mengintegrasikan api nya

---
+++ my-app/src/utils/api.jsx
@@ -0,0 +1,14 @@
+// src/utils/api.js
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
+const BASE_URL = "https://api.rawg.io/api";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    key: API_KEY,
+  },
+});
+
+export default api;
